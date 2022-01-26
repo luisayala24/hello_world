4e525e7cd7bd6e84c8dcc01de66c36f16c8ae5a3
Luis Ayala <luis.ayala2424@yahoo.com>
2022-01-26 11:44:50 -0500

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# Various ways to show "Hello World"
+
+# Common print statement
+print("Hello World!")
+
+# Combining indivdual characters
+print("H" + "e" + "l" + "l" + "o" + " " + "W" + "o" + "r" + "l" + "d" + "!")
+
+# Assigning the greeting with a single variable
+x = "Hello World!"
+print(x)
+
+# Breaking the greeting into two parts and then combining them
+a = "Hello "
+b = "World"
+c = "!"
+print(a+b+c)
